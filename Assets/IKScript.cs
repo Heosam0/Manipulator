8375112cc1a92c6f8475ef82cc6d2f6cce182799
Heosam0 <kotombor@mail.ru>
2025-06-15 17:07:25 +0500

    Текущий скрипт был переработан. Стал более универсальным.
    На сегодня принудительно заканчиваю работу, но в планах было успеть закончить с исполнительным механизмом.

--- Assets/IKScript.cs
+++ Assets/IKScript.cs
@@ -1,96 +1,82 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class IKScript : MonoBehaviour
 {
 
     [SerializeField] private float RotationSpeed = 180f;
-    [SerializeField] private Transform Base;
-    [SerializeField] private float BaseTarget, Joint1Target, Joint2Target, Joint3Target;
-    [SerializeField] private Transform Joint1, Joint2, Joint3;
-    [SerializeField] private Transform EndEffector;
-
-    [SerializeField] private Transform TargetTransform;
+    public ManipulatorJoint[] Joints;
+   
+    [SerializeField] private Transform EndEffector, TargetTransform;
     [SerializeField] private int CCDIterations = 5;
-    [SerializeField] private float CCDDamping = 0.5f;
+    [SerializeField] private float CCDDamping = 0.5f,
+        Threshold = 0.001f;
+    [SerializeField] private bool IsFollowingTarget = true;
 
-  // public void BaseEqual(float equal) => BaseTarget = equal;
-  // public void Joint1Equal(float equal) => Joint1Target = equal;
-  // public void Joint2Equal(float equal) => Joint2Target = equal;
-  // public void Joint3Equal(float equal) => Joint3Target = equal;
-
+    public void BaseEqual(float angle) => Joints[0].SetTargetAngle(angle);
+    public void SetFollowingState(bool state) => IsFollowingTarget = state;
+    public void Joint1Equal(float angle) => Joints[1].SetTargetAngle(angle);
+    public void Joint2Equal(float angle) => Joints[2].SetTargetAngle(angle);
+    public void Joint3Equal(float angle) => Joints[3].SetTargetAngle(angle);
 
     private void Update()
     {
-        CalculateAnglesByCCD();
+        if (IsFollowingTarget)
+            CalculateAnglesByCCD();
 
-        Base.localEulerAngles = new Vector3(0, NewBaseAngle(BaseTarget, Base), 0);
-        Joint1.localEulerAngles = new Vector3(NewJointAngle(Joint1Target, Joint1),0,0);
-        Joint2.localEulerAngles = new Vector3(NewJointAngle(Joint2Target, Joint2),0,0);
-        Joint3.localEulerAngles = new Vector3(NewJointAngle(Joint3Target, Joint3),0,0);
+        
+            foreach(var joint in Joints)
+                joint.Transform.localEulerAngles = joint.Axis == Axis.X
+                ? new Vector3(NewJointAngle(joint), 0, 0)
+                : new Vector3(0, NewJointAngle(joint), 0);
+            
     }
 
     private void CalculateAnglesByCCD() {
 
         for (int i = 0; i < CCDIterations; i++)
-        {
-            UpdateTargetAngle(Joint3, ref Joint3Target, Axis.X);
-            UpdateTargetAngle(Joint2, ref Joint2Target, Axis.X);
-            UpdateTargetAngle(Joint1, ref Joint1Target, Axis.X);
-            UpdateTargetAngle(Base, ref BaseTarget, Axis.Y);
-
-        }
+            for (int j = 0; j < Joints.Length; j++)
+                UpdateTargetAngle(ref Joints[j]);
     }
 
-    private void UpdateTargetAngle(Transform joint, ref float targetAngle, Axis axis)
+    private void UpdateTargetAngle(ref ManipulatorJoint joint)
     {
-        Vector3 toEffector = EndEffector.position - joint.position;
-        Vector3 toTarget = TargetTransform.position - joint.position;
+        Vector3 toEffector = EndEffector.position - joint.Transform.position;
+        Vector3 toTarget = TargetTransform.position - joint.Transform.position;
 
-        Vector3 rotationAxis = axis == Axis.X ? joint.right : joint.up;
+        Vector3 rotationAxis = joint.Axis == Axis.X 
+            ? joint.Transform.right 
+            : joint.Transform.up;
 
         float angle = Vector3.SignedAngle(toEffector, toTarget, rotationAxis);
         angle *= CCDDamping;
 
+        if (Mathf.Abs(angle) < Threshold)
+            return;
 
-        targetAngle = Mathf.Clamp(targetAngle + angle, -90, 90);
-
-             
-
-      //
-      //  Quaternion rotation = Quaternion.AngleAxis(angle, rotationAxis);
-      //  Vector3 newDirection = rotation * toEffector;
-      //  float deltaAngle = (axis == Axis.X) ? 
-      //      Vector3.SignedAngle(joint.right, newDirection, joint.forward) :
-      //      Vector3.SignedAngle(joint.up, newDirection, Vector3.up);
-      //
-      //  targetAngle += deltaAngle;
+        joint.TargetAngle = Mathf.Clamp(joint.TargetAngle + angle, -90, 90);
 
     }
-
-
-
-   
-    private float NewJointAngle(float angleTarget,Transform joint)
+       
+    private float NewJointAngle(ManipulatorJoint joint)
     {
-        float currentAngle = joint.localEulerAngles.x;
-        float angleDiff = Mathf.DeltaAngle(currentAngle, angleTarget);
+        float currentAngle = joint.Axis == Axis.X ? joint.Transform.localEulerAngles.x : joint.Transform.localEulerAngles.y;
+        float angleDiff = Mathf.DeltaAngle(currentAngle, joint.TargetAngle);
         float step = RotationSpeed * Time.deltaTime;
         return currentAngle + Mathf.Clamp(angleDiff, -step, +step);
     }
-    private float NewBaseAngle(float angleTarget, Transform joint)
-    {
-        float currentAngle = joint.localEulerAngles.y;
-        float angleDiff = Mathf.DeltaAngle(currentAngle, angleTarget);
-        float step = RotationSpeed * Time.deltaTime;
-        return currentAngle + Mathf.Clamp(angleDiff, -step, +step);
-    }
+  
+}
 
+public enum Axis { X, Y }
 
+[Serializable]
+public class ManipulatorJoint
+{
+    public Transform Transform;
+    public float TargetAngle;
+    public Axis Axis;
 
-
-
-    private enum Axis { X, Y }
+    public void SetTargetAngle(float targetAngle) => TargetAngle = targetAngle;
 }
 
