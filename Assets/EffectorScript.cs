d9c330e8bd06f74c2cfb26d3df0547aaaed5daa7
Heosam0 <kotombor@mail.ru>
2025-06-16 14:05:05 +0500

    Прототип готов.
    Сегодня по времени заняло час.

--- Assets/EffectorScript.cs
+++ Assets/EffectorScript.cs
@@ -1,6 +1,37 @@
+using System;
 using UnityEngine;
 
 public class EffectorScript : MonoBehaviour
 {
+    [SerializeField] private Claw[] Claws;
+
+    public void UpdateClawPosition(float value)
+    {
+        float angle = Mathf.Clamp(20, -20, value);
+        for(int i = 0; i < Claws.Length; i++) Claws[i].TargetAngle = angle;
+    }
     
+    private void Update()
+    {
+        for (int i = 0; i < Claws.Length; i++)
+            Claws[i].UpdateAngles();
+    }
+
 }
+
+[Serializable]
+public struct Claw
+{
+    public Transform Transform;
+    public float TargetAngle;
+
+    public void UpdateAngles()
+    {
+        float currentAngle = Transform.localEulerAngles.z;
+        float angleDiff = Mathf.DeltaAngle(currentAngle, TargetAngle);
+        float step = 180f * Time.deltaTime;
+        Transform.localEulerAngles = new Vector3(0, Transform.localEulerAngles.y, currentAngle + Mathf.Clamp(angleDiff, -step, +step));
+    }
+
+}
+
